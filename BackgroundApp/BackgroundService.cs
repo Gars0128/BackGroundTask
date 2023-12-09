5c92bfc9af8693575715a86a954ed8f4e6813426
Gars0128 <70803438+Gars0128@users.noreply.github.com>
2023-12-09 11:13:50 +0100

    task2

---
+++ BackgroundApp/BackgroundService.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BackgroundTaskScheduler.Services
+{
+    public class BackgroundTaskService : BackgroundService
+    {
+        private readonly ILogger<BackgroundTaskService> _logger;
+        private readonly int _interval;
+        private readonly IConfiguration _configuration;
+
+        public BackgroundTaskService(ILogger<BackgroundTaskService> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+            _interval = _configuration.GetValue<int>("BackgroundTaskInterval");
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Background task running at: {time}", DateTimeOffset.Now);
+                try
+                {
+                    // execute task here
+                    PerformBackgroundTask();
+
+                    _logger.LogInformation("Background task completed at: {time}", DateTimeOffset.Now);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "An error occurred while executing the background task.");
+                }
+
+                await Task.Delay(_interval, stoppingToken);
+            }
+        }
+
+        private void PerformBackgroundTask()
+        {
+            // simulate a task & generating random numbers
+            Random random = new Random();
+            int randomNumber = random.Next();
+            _logger.LogInformation("Generated random number: {number}", randomNumber);
+        }
+    }
+}
---
+++ BackgroundApp/Controllers/SampleController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace BackgroundTaskScheduler.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SampleController : ControllerBase
+    {
+        private readonly ILogger<SampleController> _logger;
+        private readonly IConfiguration _configuration;
+
+        public SampleController(ILogger<SampleController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        [HttpGet("start-background-task")]
+        public IActionResult StartBackgroundTask()
+        {
+            // You can trigger the background task from here if needed
+            _logger.LogInformation("API endpoint called to start the background task.");
+            // Trigger the background task logic here if required
+
+            return Ok("Background task started.");
+        }
+
+        [HttpGet("configure-interval/{interval}")]
+        public IActionResult ConfigureInterval(int interval)
+        {
+            // Example endpoint to configure the background task interval dynamically
+            _configuration["BackgroundTaskInterval"] = interval.ToString();
+            _logger.LogInformation("Background task interval updated to: {interval} milliseconds.", interval);
+
+            return Ok($"Background task interval updated to: {interval} milliseconds.");
+        }
+    }
+}
+
